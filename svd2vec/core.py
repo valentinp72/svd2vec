5b53b3bc997719d28414d12370a766b31ab87ee0
Valentin Pelloin <valentin.pelloin.etu@univ-lemans.fr>
2019-05-16 14:03:31 +0200

    classical PPMI matrix

--- svd2vec/core.py
+++ svd2vec/core.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pandas as pd
 
+from collections import OrderedDict, Counter
+
 class Utils:
+
+    def flatten(lst):
+        # Returns a flatten version of the given list
+        # All sublists are merged onto a bigger list. Non-list elements are removed
+        return [item for sublst in lst for item in sublst if isinstance(sublst, list)]
 
     def PMI(document, contexes, word, context, weight, alpha=0.75):
         frequency_word    = Utils.frequency(document, word)
@@ -44,19 +51,11 @@ class Utils:
 
     def PMI_matrix(window, vocabulary, document):
         n_vocabulary = len(vocabulary)
-        print(vocabulary)
         m = np.zeros((n_vocabulary, n_vocabulary))
         for word, context, weight in window:
             pmi = Utils.PMI(document, window, word, context, weight)
-            m[vocabulary[word]][vocabulary[context]] += weight * pmi
-        Utils.display_matrix(m, vocabulary)
+            m[vocabulary[word]][vocabulary[context]] += pmi
         return m
-
-    def display_matrix(matrix, vocabulary):
-        z = {a: e for e, a in vocabulary.items()}
-        v = [z[i] for i in sorted(vocabulary.values())]
-        df = pd.DataFrame(matrix, columns=v, index=v)
-        print(df)
 
     def weight_harmonic(word, context, dist, windowSize):
         weight = 1.0 / dist
@@ -75,9 +74,11 @@ class svd2vec:
     def __init__(self,
                  documents,
                  size=150,
+                 min_count=2,
                  window=10,
                  window_weight=WINDOW_WEIGHT_WORD2VEC,
-                 min_count=2,
+                 cds_alpha=0.75,
+                 sub_threshold=1e-5,
                  workers=10):
 
         if window_weight == svd2vec.WINDOW_WEIGHT_HARMONIC:
@@ -94,21 +95,100 @@ class svd2vec:
         else:
             raise ValueError(window + " not implemented as a window yielder")
 
+        self.window = window
+        self.build_vocabulary(documents)
 
-        self.vocabulary = self.get_vocabulary(documents)
+        self.terms_counts = self.documents_counts_terms()
+        self.weighted_count_matrix = self.skipgram_weighted_count_matrix()
 
-        for document in documents:
-            win = [w for w in window(document)]
-            m = Utils.PMI_matrix(win, self.vocabulary, document)
-            print(m)
-            input("a")
-            ctxs = [ctx for ctx in window(document)]
-            print(ctxs)
-            for word, context, weight in ctxs:
-                pmi = Utils.PMI(document, ctxs, word, context, weight)
-                print(word, context, pmi)
+        self.ppmi = self.ppmi_matrix()
 
-    def get_vocabulary(self, documents):
-        all = [e for document in documents for e in document]
-        unique = set(all)
-        return {word: i for i, word in enumerate(unique)}
+        self.display_matrix(self.ppmi)
+
+    #####
+    # Building informations matrices and variables to be used later
+    #####
+
+    def build_vocabulary(self, documents):
+        self.documents = documents
+        self.all_words = Utils.flatten(self.documents)
+        self.d_size    = len(self.all_words)
+
+        unique = list(OrderedDict.fromkeys(self.all_words))
+        self.vocabulary = OrderedDict()
+        for i in range(len(unique)):
+            self.vocabulary[unique[i]] = i
+
+        self.vocabulary_len = len(self.vocabulary)
+
+    def documents_counts_terms(self):
+        counts = Counter(self.all_words)
+        return counts
+
+    def skipgram_weighted_count_matrix(self):
+        matrix = np.zeros((self.vocabulary_len, self.vocabulary_len))
+
+        for document in self.documents:
+            for word, context, weight in self.window(document):
+                i_word    = self.vocabulary[word]
+                i_context = self.vocabulary[context]
+                matrix[i_word, i_context] += weight
+
+        return matrix
+
+    def pmi_matrix(self):
+        pmi = np.zeros((self.vocabulary_len, self.vocabulary_len))
+
+        for word in self.vocabulary:
+            for context in self.vocabulary:
+                i_word    = self.vocabulary[word]
+                i_context = self.vocabulary[context]
+                pmi[i_word, i_context] = self.pmi(word, context)
+
+        return pmi
+
+    def ppmi_matrix(self):
+        pmi  = self.pmi_matrix()
+        zero = np.zeros(pmi.shape)
+        return np.maximum(pmi, zero)
+
+    #####
+    # Getting informations
+    #####
+
+    def weight_count_term(self, term):
+        count_term = self.terms_counts[term]
+        return count_term
+
+    def weight_count_term_term(self, t1, t2):
+        i_t1 = self.vocabulary[t1]
+        i_t2 = self.vocabulary[t2]
+        weighted_count = self.weighted_count_matrix[i_t1, i_t2]
+        return weighted_count
+
+    def pmi(self, word, context):
+
+        n_wc = self.weight_count_term_term(word, context)
+        n_w  = self.weight_count_term(word)
+        n_c  = self.weight_count_term(context)
+
+        n_d = self.d_size
+
+        frac = (n_wc * n_d) / (n_w * n_c)
+
+        if frac == 0:
+            return -np.inf
+        return np.log2(frac)
+
+    #####
+    # Debug
+    #####
+
+    def display_matrix(self, matrix, vocabulary=None):
+        if vocabulary is None:
+            vocabulary = self.vocabulary
+        z = {a: e for e, a in vocabulary.items()}
+        v = [z[i] for i in sorted(vocabulary.values())]
+        df = pd.DataFrame(matrix, columns=v, index=v)
+        df = df.applymap(lambda x: '{:4.2f}'.format(x) if x != 0 else "")
+        print(df)
---
+++ tests/testFull.py
@@ -0,0 +1,9 @@
+
+from context import svd2vec
+
+
+documents = [open("/home/s150789/OP00575V_1_29012008.txt", "r").read().lower().splitlines()]
+
+print(documents)
+
+svd2vec.svd2vec(documents, window=5, window_weight=svd2vec.svd2vec.WINDOW_WEIGHT_WORD2VEC)
