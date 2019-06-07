93df4ff58b5ea91d05d9b45ab9473be2a2edd77f
Valentin Pelloin <valentin.pelloin.etu@univ-lemans.fr>
2019-06-07 14:29:28 +0200

    with mirroring script

--- mirror_github.sh
+++ mirror_github.sh
@@ -6,3 +6,8 @@ cd svd2vec.git
 git push --mirror git@github.com:valentinp72/svd2vec.git
 cd ..
 rm -rf svd2vec.git
+git clone git@github.com:valentinp72/svd2vec.git
+cd svd2vec
+git subtree push --prefix docs/build/html origin gh-pages
+cd ..
+rm -rf svd2vec
