674e543b7ddb33a92597d6c1cd8710cad3743111
Valentin Pelloin <valentin.pelloin.etu@univ-lemans.fr>
2019-06-07 15:18:21 +0200

    documentation link - pip version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='svd2vec',
-      version='0.1.1',
+      version='0.2',
       description='A library that converts words to vectors using PMI and SVD',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -14,8 +14,8 @@ setup(name='svd2vec',
       license='MIT',
       packages=['svd2vec'],
       package_data={'svd2vec': ['datasets/similarities/*.txt', 'datasets/analogies/*.txt']},
-      zip_safe=False,
-      classifiers=["Programming Language :: Python :: 3",
-                   "License :: OSI Approved :: MIT License",
-                   "Operating System :: OS Independent",
-      ])
+      classifiers=[
+                  "Programming Language :: Python :: 3",
+                  "License :: OSI Approved :: MIT License",
+                  "Operating System :: OS Independent",]
+      )
