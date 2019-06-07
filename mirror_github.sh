64b3746b75bd1c8128266ea43931af5a6d366696
Valentin Pelloin <valentin.pelloin.etu@univ-lemans.fr>
2019-06-07 13:43:18 +0200

    with mirroring script

---
+++ mirror_github.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd /tmp
+git clone --bare git@git-lium.univ-lemans.fr:vpelloin/svd2vec.git
+cd svd2vec.git
+git push --mirror git@github.com:valentinp72/svd2vec.git
+cd ..
+rm -rf svd2vec.git
